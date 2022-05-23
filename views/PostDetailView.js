e8b1b0c7ba2a0af11815306994d9bf625c79ec72
Fatih Yarlıgan <fatih.yarligan@bahcesehir.edu.tr>
2022-05-23 22:51:06 +0300

    vertical table

--- views/PostDetailView.js
+++ views/PostDetailView.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react'
-import { View, Text, Button, FlatList, ActivityIndicator, ScrollView } from 'react-native';
+import { View, Text, Button, FlatList, ActivityIndicator, ScrollView, StyleSheet } from 'react-native';
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
-function ObjToArr(Obj) {
-    if (typeof (Obj) == "object") {
-        let Arr = Object.keys(Obj).map(function (key) {
-            return " " + ObjToArr(Obj[key])
-        });
-        return Arr;
-    } else {
-        return Obj;
-    }
+function ObjToArr(Obj){
+  if(typeof(Obj)=="object"){
+    let Arr = Object.keys(Obj).map(function(key) {
+      return " "+ObjToArr(Obj[key])
+    });
+    return Arr;
+  }else{
+    return Obj;
+  }
 }
 const UserDetailView = (props) => {
     const { goForFetch, fromFetch, dataSource, loading } = props
     let HeadTable = Object.keys(dataSource)
-    let DataTable = ObjToArr(dataSource)
+    let DataTable = ObjToArr(dataSource).map(a => [a])
+    let DataHeights = [28,28,56,224] //can be rendered by data length
     //let DataTable = Object.values(dataSource).slice(0,4) // [5] error
     return (
         <View>
@@ -28,29 +29,32 @@ const UserDetailView = (props) => {
             </View>
 
             <View style={{ margin: 18, textAlign: 'center' }}>
-                {fromFetch ?
-                    <ScrollView horizontal={true}>
-                        <Table borderStyle={{ borderWidth: 1, borderColor: 'blue' }}>
-                            <TableWrapper>
-                                <Row data={HeadTable} />
-                                <Rows data={[DataTable]} />
-                                { /* //TODO Horizontal table
-                    <Cols data={DataTable.map(a => [a])} />
-                    */ }
-                            </TableWrapper>
-                        </Table>
-
-                    </ScrollView> :
-                    "no data yet"
-                }
-                {loading &&
-                    <View>
-                        <ActivityIndicator size="large" color="#0c9" />
-                        <Text>Fetching Data</Text>
-                    </View>
-                }
-            </View>
+            {fromFetch ?
+              <ScrollView vertical={true}>
+                <Table borderStyle={{borderWidth: 1}}>
+                  <TableWrapper style={styles.wrapper}>
+                    <Col data={HeadTable} style={styles.title} heightArr={DataHeights} textStyle={styles.text}/>
+                    <Rows data={DataTable} flexArr={[2, 1]} heightArr={DataHeights} textStyle={styles.text}/>
+                  </TableWrapper>
+                </Table>
+              </ScrollView> :
+              "no data yet"
+            }
+            {loading &&
+              <View>
+                  <ActivityIndicator size="large" color="#0c9" />
+                  <Text>Fetching Data</Text>
+              </View>
+            }
+        </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+  wrapper: { flexDirection: 'row' },
+  title: { flex: 1, backgroundColor: '#f6f8fa', width: 70 },
+  text: { textAlign: 'center' }
+});
+
 export default UserDetailView;
