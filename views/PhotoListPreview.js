4290bc98f3d87adc13f5aa354f73c87a0c85ef9a
sedefsayibasi <sedefsayibasi@gmail.com>
2022-06-16 11:19:20 +0300

    Photo List Page

---
+++ views/PhotoListPreview.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  Platform,
+} from 'react-native';
+
+const PreView = (props) => {
+  const { style, item, imageKey, onPress, index, active, local } = props;
+
+  return (
+    <TouchableOpacity
+      style={[styles.videoContainer]}
+      onPress={() => onPress(item)}>
+      <View style={[styles.imageContainer, styles.shadow]}>
+        <Image
+          style={[styles.videoPreview, active ? {} : {height: 120}]}
+          source={{uri: item[imageKey]}}
+        />
+      </View>
+      <Text style={styles.desc}>{item.desc}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  videoContainer: {
+    width: 275,
+    paddingVertical: 28,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 20,
+  },
+  videoPreview: {
+    width: 275,
+    height: 155,
+    borderRadius: 8,
+    resizeMode: 'cover',
+  },
+  desc: {
+    fontSize: 14,
+    letterSpacing: 0,
+    lineHeight: 24,
+    marginTop: 18,
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  shadow: {
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 1},
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+});
+export default PreView;
--- views/PhotoListView.js
+++ views/PhotoListView.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react'
-import { View, Text, Button, FlatList, ActivityIndicator } from 'react-native';
+import { View, Text, Button, ActivityIndicator } from 'react-native';
+import {FlatListSlider} from 'react-native-flatlist-slider';
+import Preview from '../views/PhotoListPreview';
+
 const PhotoListView = (props) => {
+    const { goForFetch, fromFetch, renderItem, FlatListItemSeparator, dataSource, loading, completed } = props;
+    console.log("dataSource :");
+    console.log(dataSource);
     return (
         <View>
+            <View style={{ margin: 18, display: 'none' }}>
+                <Button
+                    title={'Get Photo List'}
+                    onPress={goForFetch}
+                    color='blue'
+                />
+            </View>
+
+            <View style={{ margin: 18, textAlign: 'center' }}>
+                {
+                  completed ?
+                  <View>
+                    {console.log("completed dataSource")}
+                    <FlatListSlider
+                      data={dataSource}
+                      width={275}
+                      timer={5000}
+                      component={<Preview />}
+                      contentContainerStyle={{paddingHorizontal: 16}}
+                    />
+                  </View> : 'no data yet'
+                }
+                {
+                  loading &&
+                    <View>
+                        <ActivityIndicator size="large" color="#0c9" />
+                        <Text>Fetching Data</Text>
+                    </View>
+                }
+            </View>
         </View>
     )
 }
