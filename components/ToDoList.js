5f0144b8e422860312f2341150c8444a7f343f4a
Fatih Yarlıgan <fatih.yarligan@bahcesehir.edu.tr>
2022-06-17 02:21:15 +0300

    ToDo List Page

---
+++ components/ToDoList.js
@@ -0,0 +1,120 @@
+import React, { Component } from 'react';
+import ToDoListView from '../views/ToDoListView';
+import {
+    StyleSheet,
+    View,
+    ActivityIndicator,
+    FlatList,
+    Text,
+    TouchableOpacity
+} from "react-native";
+
+class ToDoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            fromFetch: false,
+            dataSource: [],
+        };
+    }
+    static getDerivedStateFromProps(props, state) {
+        let _userid = state.userid;
+        if (typeof props.userid !== 'undefined') {
+            _userid = props.userid
+        }
+        if (_userid != state.userid) {
+            console.log('changed userid in ToDolist : ', _userid);
+            //this.goForFetch() // Error, so =>componentDidUpdate(prevProps)
+        }
+        return { userid: _userid };
+    }
+    componentDidUpdate(prevProps, prevState) { // does not detecting
+        if (prevState.userid !== this.state.userid) {
+            console.log('change detected userid in ToDolist : ', this.state.userid);
+            let fetchUrl = "https://jsonplaceholder.typicode.com/todos?userId=" + this.state.userid;
+            console.log(fetchUrl);
+            this.goForFetch(fetchUrl);
+        }
+    }
+    componentDidMount() {
+        this.goForFetch();
+    }
+    goForFetch = (fetchUrl = "https://jsonplaceholder.typicode.com/todos") => {
+        this.setState({
+            fromFetch: true,
+            loading: true,
+        })
+        console.log(fetchUrl);
+        if (typeof this.state.userid !== 'undefined') {
+            fetchUrl = fetchUrl + "?userId=" + this.state.userid;
+        }
+        console.log(fetchUrl);
+
+        fetch(fetchUrl)
+            .then(response => response.json())
+            .then((responseJson) => {
+                console.log('getting data from api', responseJson)
+                this.setState({
+                    loading: false,
+                    dataSource: responseJson.slice(0, 20),
+                })
+            })
+            .catch(error => console.log(error))
+    }
+    FlatListSeparator = () => {
+        return (
+            <View style={{
+                height: .5,
+                width: "100%",
+                backgroundColor: "rgba(0,0,0,0.5)",
+            }}
+            />
+        );
+    }
+    renderItem = (data) => {
+        return (
+            <TouchableOpacity
+                style={[{ padding: 5 }, data.item.id % 2 && { backgroundColor: '#ffffff' }]}
+                onPress={() => {
+
+                    console.log('pressed id :', data.item.id);
+                }}
+            >
+              <View style={{flex: 1, flexDirection: 'row', justifyContent: "start"}}>
+                <Text style={{fontSize: 20}}>
+                {
+                  data.item.completed ?
+                    '☑'
+                  :
+                    '☐'
+                    // '☒'
+                }
+                </Text>
+                <Text style={{margin:5, textAlign:"left"}}>{data.item.title}</Text>
+              </View>
+            </TouchableOpacity>
+        )
+
+    }
+    render() {
+        const { dataSource, fromFetch, loading } = this.state
+        return (
+            <View>
+                <Text style={{ fontWeight: 'bold', textAlign: 'center' }}>
+                    {this.props.title}
+                </Text>
+                <ToDoListView
+                    goForFetch={this.goForFetch}
+                    dataSource={dataSource}
+                    loading={loading}
+                    fromFetch={fromFetch}
+                    FlatListSeparator={this.FlatListSeparator}
+                    renderItem={this.renderItem}
+                />
+            </View>
+        );
+    }
+}
+
+export default ToDoList;
--- components/UserDetail.js
+++ components/UserDetail.js
@@ -1,89 +1,95 @@
 import React, { Component } from 'react';
 import UserDetailView from '../views/UserDetailView';
 import PostList from './PostList';
+import AlbumList from './AlbumList';
+import ToDoList from './ToDoList';
 
 import {
-    StyleSheet,
-    View,
-    Text
+  StyleSheet,
+  View,
+  Text
 } from "react-native";
 
 class UserDetail extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            displayExtra: (typeof this.props.displayExtra !== 'undefined') ? this.props.displayExtra : true,
-            loading: false,
-            fromFetch: false,
-            dataSource: [],
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayExtra: (typeof this.props.displayExtra !== 'undefined') ? this.props.displayExtra : true,
+      loading: false,
+      fromFetch: false,
+      dataSource: [],
+    };
+  }
 
-    static getDerivedStateFromProps(props, state) {
-        let _userid = state.userid;
-        if (typeof props.route !== 'undefined') {
-            _userid = props.route.params.userid
-        } else if (typeof props.userid !== 'undefined') {
-            _userid = props.userid
-        }
-        if (_userid != state.userid) {
-            console.log('changed userid in getDerivedStateFromProps : ', _userid);
-            //this.goForFetch() // Error, so =>componentDidUpdate(prevProps)
-        }
-        return { userid: _userid };
+  static getDerivedStateFromProps(props, state) {
+    let _userid = state.userid;
+    if (typeof props.route !== 'undefined') {
+      _userid = props.route.params.userid
+    } else if (typeof props.userid !== 'undefined') {
+      _userid = props.userid
     }
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.userid !== this.state.userid) {
-            console.log('change detected userid in componentDidUpdate : ', this.state.userid);
-            this.goForFetch(); //example calling redux action
-        }
+    if (_userid != state.userid) {
+      console.log('changed userid in getDerivedStateFromProps : ', _userid);
+      //this.goForFetch() // Error, so =>componentDidUpdate(prevProps)
     }
+    return { userid: _userid };
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.userid !== this.state.userid) {
+      console.log('change detected userid in componentDidUpdate : ', this.state.userid);
+      this.goForFetch(); //example calling redux action
+    }
+  }
 
-    //componentWillMount is unnecessary
-    componentDidMount() {
-        this.goForFetch()
-    }
-    goForFetch = () => {
+  //componentWillMount is unnecessary
+  componentDidMount() {
+    this.goForFetch()
+  }
+  goForFetch = () => {
+    this.setState({
+      fromFetch: true,
+      loading: true,
+    })
+    fetch("https://jsonplaceholder.typicode.com/users/" + this.state.userid)
+      .then(response => response.json())
+      .then((responseJson) => {
+        console.log('getting data from api', responseJson)
         this.setState({
-            fromFetch: true,
-            loading: true,
+          loading: false,
+          dataSource: responseJson
         })
-        fetch("https://jsonplaceholder.typicode.com/users/" + this.state.userid)
-            .then(response => response.json())
-            .then((responseJson) => {
-                console.log('getting data from api', responseJson)
-                this.setState({
-                    loading: false,
-                    dataSource: responseJson
-                })
-            })
-            .catch(error => console.log(error))
-    }
+      })
+      .catch(error => console.log(error))
+  }
 
-    render() {
-        const { dataSource, fromFetch, loading, displayExtra } = this.state
-        return (
-            <View>
+  render() {
+    const { dataSource, fromFetch, loading, displayExtra } = this.state
+    return (
+      <View>
 
-                <Text style={{ fontWeight: 'bold', textAlign: 'center' }}>
-                    {this.props.title}
-                </Text>
-                <UserDetailView
-                    goForFetch={this.goForFetch}
-                    dataSource={dataSource}
-                    loading={loading}
-                    fromFetch={fromFetch}
-                />
+        <Text style={{ fontWeight: 'bold', textAlign: 'center' }}>
+          {this.props.title}
+        </Text>
+        <UserDetailView
+          goForFetch={this.goForFetch}
+          dataSource={dataSource}
+          loading={loading}
+          fromFetch={fromFetch}
+        />
 
-                {
-                    displayExtra ?
-                        <PostList userid={this.state.userid} title="Created Posts" navigation={this.props.navigation} route={this.props.route} />
-                        :
-                        ""
-                }
-            </View>
-        );
-    }
+        <ToDoList userid={this.state.userid} title="To Do List" navigation={this.props.navigation} />
+
+        <AlbumList userid={this.state.userid} title="Created Albums" navigation={this.props.navigation} />
+
+        {
+          displayExtra ?
+            <PostList userid={this.state.userid} title="Created Posts" navigation={this.props.navigation} />
+            :
+            ""
+        }
+      </View>
+    );
+  }
 }
 
 export default UserDetail;
